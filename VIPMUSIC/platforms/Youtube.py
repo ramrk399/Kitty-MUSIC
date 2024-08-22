1f347eb266281ce3db63031fc9f0dd2e88a6d828
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-22 11:33:39 +0530

    Update Youtube.py

--- VIPMUSIC/platforms/Youtube.py
+++ VIPMUSIC/platforms/Youtube.py
@@ -337,7 +337,7 @@ class YouTubeAPI:
             fpath = f"downloads/{title}.mp3"
             return fpath
         elif video:
-            if await is_on_off(2):
+            if await is_on_off(1):
                 direct = True
                 downloaded_file = await loop.run_in_executor(None, video_dl)
             else:
