06cf73078b1d7daf28ea962b5d8674af077d4e24
ramrk399 <ramavtarchauhan2000@gmail.com>
2024-08-22 23:57:35 +0530

    Update gitinfo.py

--- VIPMUSIC/plugins/tools/gitinfo.py
+++ VIPMUSIC/plugins/tools/gitinfo.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git RAJPUT-SHIV-SINGH")
+        await message.reply_text("/git -ll_THUNDER_lll")
         return
 
     username = message.text.split(None, 1)[1]
